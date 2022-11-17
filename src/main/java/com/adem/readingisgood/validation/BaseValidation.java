57264ce7edc88d5982ae7bbcd183a962905b3e26
adem0zer <43863411+adem0zer@users.noreply.github.com>
2022-11-18 00:56:49 +0300

    refactor

--- src/main/java/com/adem/readingisgood/validation/BaseValidation.java
+++ src/main/java/com/adem/readingisgood/validation/BaseValidation.java
@@ -2,5 +2,5 @@ package com.adem.readingisgood.validation;
 
 
 public interface BaseValidation {
-    public void valid(Object... valid) throws Exception;
+    void valid(Object... valid) throws Exception;
 }
